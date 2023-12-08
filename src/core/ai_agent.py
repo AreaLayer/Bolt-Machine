8aad92d6a229c0cc6192f5c00d40d59ae4c1e98e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-12-08 09:00:05 -0300

    Update ai_agent.py

--- src/core/ai_agent.py
+++ src/core/ai_agent.py
@@ -8,6 +8,15 @@ import nest_asyncio
 
 nest_asyncio.apply()
 
+def data (a:int , b: int) -> int:
+    "multiple push data around liquidity"
+
+def inbound (a: int , b: int) -> int:
+    "multiple data about inbound liquidity"
+
+def outbound
+(a: int , b: int) -> int:
+    "multiple data about outbound liquidity"
 
 
 def multiply(a: int, b: int) -> int:
