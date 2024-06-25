bdc82b37226658d7494a7d3cfa339c23a858a177
223880 <rafael.segala45@gmail.com>
2024-06-25 08:16:56 -0300

    Add main and LND with macroon

--- lightning.go
+++ lightning.go
@@ -1,1 +1,19 @@
+import { UXTO, OpenChannel, CloseChannel, SendPayment, GetInfo, GetPeer } from 'LND/lnd_methods';7
+import { Lnd } from 'LND/lnd_grpc';
+
+const lnd = new Lnd({
+  cert_macaroon_path: '/path/to/admin.macaroon',
+  lnd_dir: '/path/to/lnd/',
+  network:'testnet', ## You can switch to signet/mainnet 
+  case condition
+});
+
+// Get info about the node7
+lnd.getInfo()
+ .then(info => {
+	console.log(info);
+  })
+ .catch(err => {
+    console.log(err);
+  });
 
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
+import "fmt"
+import "main"
+
+func main() {
+	fmt.Println("Hello, world")
+}
+
+func foo() {
+	fmt.Println("Hello, world")
+}
+
+func bar() {
+	fmt.Println("Hello, world")
+}
+
+fn main () {
+	fmt.Println("Hello, world")
+}
+
 
