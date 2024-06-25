d9de780a27cfe2dbfcc632620d0319f431bfd9c3
223880 <rafael.segala45@gmail.com>
2024-06-25 08:30:09 -0300

    Fix

--- state.go
+++ state.go
@@ -1,1 +1,18 @@
+import { State } from './state';
+import { StateMachine } from './statemachine';
+
+export { State, StateMachine };
+
+fn main() {
+
+	let mut sm = StateMachine::new();
+	sm.run();
+}
+
+fn run() {
+	main();
+}
+
+run();
+
 
--- ui.go
+++ ui.go
@@ -1,2 +1,22 @@
-import {
+import { UserInterface } from './user.interface';
+import { User } from './user.model';
+import { UserService } from './user.service';
+
+export const UserProviders = [
+  {
+    provide: UserInterface,
+    useClass: UserService,
+  },
+];
+
+fn main() {
+  const
+  user = new User(),
+  userService = new UserService();
+  userService.create(user);
+  console.log(user);
+  userService.findAll();
+  console.log(user);
+}
+
   
