1dd9b2f30e8292b43efe3950c67727998213fed3
223880 <rafael.segala45@gmail.com>
2024-11-11 06:08:48 -0300

    Fix LDK

--- src/core/ldk.py
+++ src/core/ldk.py
@@ -28,3 +28,12 @@ def get_transactions(address):
         Returns:
             list: A list of transactions associated with the address.
         """
+
+def get_address_balance(address):
+    """
+    Retrieves the balance of a Bitcoin address.
+    Args:
+        address (str): The Bitcoin address.
+    Returns:
+        float: The balance of the address.
+    """
